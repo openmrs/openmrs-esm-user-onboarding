7391a9fa38ba1a72f2530b48aa31373548218a62
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2024-06-25 10:51:41 +0100

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+/**
+ * @returns {Promise<import('jest').Config>}
+ */
+const path = require('path');
+
+module.exports = {
+  collectCoverageFrom: [
+    '**/src/**/*.component.tsx',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/src/**/*.test.*',
+    '!**/src/declarations.d.ts',
+    '!**/e2e/**',
+  ],
+  transform: {
+    '^.+\\.tsx?$': ['@swc/jest'],
+  },
+  transformIgnorePatterns: ['/node_modules/(?!@openmrs)'],
+  moduleNameMapper: {
+    '@openmrs/esm-framework': '@openmrs/esm-framework/mock',
+    '@openmrs/esm-utils': '@openmrs/esm-framework/mock',
+    '\\.(s?css)$': 'identity-obj-proxy',
+    '^lodash-es/(.*)$': 'lodash/$1',
+    'lodash-es': 'lodash',
+    '^dexie$': require.resolve('dexie'),
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setup-tests.ts'],
+  testPathIgnorePatterns: [path.resolve(__dirname, 'e2e')],
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: 'http://localhost/',
+  },
+};

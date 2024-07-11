9a051e10148bc25b3a038c107931bec7b59c213f
jayasanka <33048395+jayasanka-sack@users.noreply.github.com>
2024-07-11 14:13:19 +0100

    Remove the context

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactJoyride from 'react-joyride';
-import { useOnboarding } from './onboarding-context';
 
 const RootComponent: React.FC = () => {
-  const { runJoyride, steps } = useOnboarding();
-  return <ReactJoyride steps={steps} run={runJoyride} />;
+  return <ReactJoyride steps={[]} run={false} />;
 };
 export default RootComponent;
--- src/tutorial/modal.component.tsx
+++ src/tutorial/modal.component.tsx
@@ -2,27 +2,24 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Modal } from '@carbon/react';
 import { useConfig } from '@openmrs/esm-framework';
-import { useOnboarding } from '../onboarding-context';
 import styles from './styles.scss';
 
 const TutorialModal = ({ open, onClose }) => {
   const { t } = useTranslation();
   const config = useConfig();
-  const tutorialData = config.tutorialData;
-  const { startOnboarding } = useOnboarding();
+  const tutorials = config.tutorialData;
 
   const handleWalkthroughClick = (steps) => {
-    startOnboarding(steps);
     onClose();
   };
-  
+
   return (
     <Modal open={open} onRequestClose={onClose} passiveModal modalHeading={t('tutorial', 'Tutorials')}>
       <p className={styles.description}>
         {t('modalDescription', 'Find walkthroughs and video tutorials on some of the core features of OpenMRS.')}
       </p>
       <div className={styles.tutorialModal}>
-        {tutorialData.map((tutorial, index) => (
+        {tutorials.map((tutorial, index) => (
           <div className={styles.tutorialItem} key={index}>
             <h3 className={styles.tutorialTitle}>{tutorial.title}</h3>
             <p className={styles.tutorialDescription}>{tutorial.description}</p>

0c99405f3f7f50ecdcf82b6588db808256efa6d7
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2024-07-12 13:30:40 +0100

    Make tutorials re-runnable

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactJoyride, {type Step} from 'react-joyride';
-import {useAppContext, useDefineAppContext} from "@openmrs/esm-framework"
+import ReactJoyride, {ACTIONS, type CallBackProps, EVENTS, type Step} from 'react-joyride';
+import {useDefineAppContext} from "@openmrs/esm-framework"
 import {type TutorialContext} from "./types";
 
 const RootComponent: React.FC = () => {
 
   const [showTutorial, setShowTutorial] = React.useState(false);
   const [steps, setSteps] = React.useState([]);
+  const [stepIndex, setStepIndex] = React.useState(0);
 
   useDefineAppContext<TutorialContext>('tutorial-context', {
     showTutorial,
@@ -15,6 +16,36 @@ const RootComponent: React.FC = () => {
     setSteps: (steps: Step[]) => setSteps(steps)
   });
 
-  return <ReactJoyride steps={steps} run={showTutorial}/>;
+  const handleJoyrideCallback = (data: CallBackProps) => {
+    const {action, index, origin, status, type} = data;
+
+    switch (type) {
+      case EVENTS.STEP_AFTER:
+      case EVENTS.TARGET_NOT_FOUND:
+        setStepIndex(index + (action === ACTIONS.PREV ? -1 : 1));
+        break;
+      case EVENTS.TOUR_END:
+        setStepIndex(0)
+        setShowTutorial(false);
+        break;
+    }
+  }
+
+  return <ReactJoyride
+    continuous
+    debug
+    disableScrolling
+    showProgress
+    showSkipButton
+    steps={steps}
+    stepIndex={stepIndex}
+    run={showTutorial}
+    callback={handleJoyrideCallback}
+    styles={{
+      options: {
+        zIndex: 10000,
+      },
+    }}
+  />;
 };
 export default RootComponent;

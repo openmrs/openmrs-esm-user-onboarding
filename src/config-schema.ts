e23d858fab4ddefbb4698421f15075064bf479dd
Jayasanka Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2024-07-26 14:32:51 +0100

    (feat) Add the ability to wait for elements to appear and automatic steps (#10)

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -40,16 +40,62 @@ export const configSchema = {
         ],
       },
       {
-        title: 'Option Tutorial',
-        description: 'Optional Tutorial for debugging purposes',
-        steps: [{
-          target: '[aria-label="OpenMRS"]',
-          content: 'Welcome to OpenMRS! This is the main dashboard where you can navigate to various features of the system.'
-        },
+        title: 'Tutorial for demo purposes',
+        description: 'This tutorial is for demo / debugging purposes only',
+        steps: [
           {
-            target: '[name="SearchPatientIcon"]',
-            content: 'This is the search icon. Use it to find patients in the system quickly.'
-          }]
+            target: '[aria-label="OpenMRS"]',
+            title: 'Let us walk through the tutorial features together.',
+            disableBeacon: true,
+          },
+          {
+            target: '[data-extension-id="clinical-appointments-dashboard-link"]',
+            title: 'Click on this link. This step is configured to be automatic and will take you to the next step. Once the given query selector resolves an element on the page, it will proceed automatically.',
+            hideCloseButton: true,
+            hideNextButton: true,
+            disableOverlayClose: true,
+            spotlightClicks: true,
+            data: {
+              autoNextOn: '[data-extension-id="clinical-appointments-dashboard"]',
+            }
+          },
+          {
+            target: '[data-extension-id="clinical-appointments-dashboard"]',
+            title: 'Congrats! You have reached the clinical appointments dashboard.',
+            disableOverlayClose: true,
+          },
+          {
+            target: '[aria-label="OpenMRS"]',
+            title: 'Now, let’s see how this behaves when elements take a bit longer to load. Set your network throttling to "Slow 3G" and hit "Next".',
+            disableOverlayClose: true,
+            hideBackButton: true,
+          },
+          {
+            target: '[data-extension-id="laboratory-dashboard-link"]',
+            title: 'Let\'s navigate to the laboratory page. Our next target is the "Tests Ordered" table. I’ll disappear once you reach the laboratory page and reappear when the table is loaded. See you there!',
+            hideCloseButton: true,
+            hideNextButton: true,
+            disableOverlayClose: true,
+            spotlightClicks: true,
+            data: {
+              autoNextOn: '[data-extension-id="laboratory-dashboard"]',
+            }
+          },
+          {
+            target: '[data-extension-id="all-lab-requests-table"] table',
+            title: 'It\'s me again. By default, I\'ll wait for the element to appear, so you don\'t have to worry about slow components when writing a new tutorial.',
+            disableOverlayClose: true,
+            hideBackButton: true,
+          },
+          {
+            target: '[aria-label="OpenMRS"]',
+            title: 'Now let\'s do a fun exercise. Can you find out how to view a patient\'s allergies on your own? Feel free to turn off network throttling. ;) ',
+          },
+          {
+            target: '[data-extension-slot-name="patient-chart-allergies-dashboard-slot"]',
+            title: 'Great job! You found the allergies section! This is the end of the tutorial. Feel free to explore the system on your own or check out the other tutorials.',
+          }
+        ]
       }
     ]
   }
@@ -64,6 +110,9 @@ export type Config = {
       target: string;
       content: string;
       disableBeacon: boolean;
+      data?: {
+        autoNextOn?: boolean;
+      }
     }[];
   }[];
 };
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactJoyride, {ACTIONS, type CallBackProps, EVENTS, type Step} from 'react-joyride';
-import {useDefineAppContext} from "@openmrs/esm-framework"
-import {type TutorialContext} from "./types";
+import ReactJoyride, { ACTIONS, type CallBackProps, EVENTS, type Step } from 'react-joyride';
+import { useDefineAppContext } from '@openmrs/esm-framework';
+import { type TutorialContext } from './types';
 
 const RootComponent: React.FC = () => {
 
   const [showTutorial, setShowTutorial] = React.useState(false);
-  const [steps, setSteps] = React.useState([]);
+  const [steps, setSteps] = React.useState<Step[]>([]);
   const [stepIndex, setStepIndex] = React.useState(0);
 
   useDefineAppContext<TutorialContext>('tutorial-context', {
@@ -16,13 +16,52 @@ const RootComponent: React.FC = () => {
     setSteps: (steps: Step[]) => setSteps(steps)
   });
 
+  const onStepChange = (index: number) => {
+    const step = steps[index];
+    if (step.data && step.data.autoNextOn) {
+      handleAutoNext(step.data.autoNextOn, index);
+    }
+  }
+
+  const waitForTarget = (index: number) => {
+    setShowTutorial(false);
+    const interval = setInterval(() => {
+      const targetElement = document.querySelector(steps[index].target as string);
+      if (targetElement) {
+        setShowTutorial(true);
+        clearTimeout(interval);
+      }
+    }, 1000);
+
+  };
+
+  const handleAutoNext = (query: string, index: number) => {
+    const interval = setInterval(() => {
+      const targetElement = document.querySelector(query);
+      if (targetElement) {
+        setStepIndex(index + 1);
+        clearTimeout(interval);
+      }
+    }, 1000);
+  }
+
+
+
   const handleJoyrideCallback = (data: CallBackProps) => {
     const {action, index, origin, status, type} = data;
-
     switch (type) {
+      case EVENTS.TOUR_START:
+        // The target not found event is not triggered when the tour starts
+        waitForTarget(0);
+        break;
+      case EVENTS.STEP_BEFORE:
+        onStepChange(index);
+        break;
       case EVENTS.STEP_AFTER:
-      case EVENTS.TARGET_NOT_FOUND:
         setStepIndex(index + (action === ACTIONS.PREV ? -1 : 1));
+        break;
+      case EVENTS.TARGET_NOT_FOUND:
+        waitForTarget(index);
         break;
       case EVENTS.TOUR_END:
         setStepIndex(0)
@@ -31,21 +70,24 @@ const RootComponent: React.FC = () => {
     }
   }
 
-  return <ReactJoyride
-    continuous
-    debug
-    disableScrolling
-    showProgress
-    showSkipButton
-    steps={steps}
-    stepIndex={stepIndex}
-    run={showTutorial}
-    callback={handleJoyrideCallback}
-    styles={{
-      options: {
-        zIndex: 10000,
-      },
-    }}
-  />;
+  return (
+    <ReactJoyride
+      continuous
+      debug
+      disableScrolling
+      showProgress
+      showSkipButton
+      steps={steps}
+      stepIndex={stepIndex}
+      run={showTutorial}
+      callback={handleJoyrideCallback}
+      styles={{
+        options: {
+          zIndex: 10000,
+        },
+        overlay: { height: document.body.scrollHeight },
+      }}
+    />
+  );
 };
 export default RootComponent;

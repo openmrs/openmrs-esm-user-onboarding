bc6b0624f1906c43261bda3742be58e72708d328
vijay <vijaykv2228@gmail.com>
2024-07-26 20:31:29 +0530

    Add patient tutorial

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -12,11 +12,14 @@ export const configSchema = {
     _default: [
       {
         title: 'Basic Tutorial',
-        description: 'Learn how to efficiently search for patients, register new patients, access user settings, and view ongoing visits and appointments.',
-        steps: [{
-          target: '[aria-label="OpenMRS"]',
-          content: 'Welcome to OpenMRS! This is the main dashboard where you can navigate to various features of the system.'
-        },
+        description:
+          'Learn how to efficiently search for patients, register new patients, access user settings, and view ongoing visits and appointments.',
+        steps: [
+          {
+            target: '[aria-label="OpenMRS"]',
+            content:
+              'Welcome to OpenMRS! This is the main dashboard where you can navigate to various features of the system.',
+          },
           {
             target: '[name="SearchPatientIcon"]',
             content: 'This is the search icon. Use it to find patients in the system quickly.',
@@ -109,67 +112,54 @@ export const configSchema = {
           },
         ],
       },
-
       {
-        title: 'Tutorial for demo purposes',
-        description: 'This tutorial is for demo / debugging purposes only',
+        title: 'Patient Registration Tutorial',
+        description: 'Learn how to register a new patient into the system.',
         steps: [
           {
-            target: '[aria-label="OpenMRS"]',
-            title: 'Let us walk through the tutorial features together.',
+            target: '[name="AddPatientIcon"]',
+            title: 'Add Patient',
+            content: 'Click here to add a patient to the system.',
+            disableBeacon: true,
+            disableOverlayClose: true,
+            spotlightClicks: true,
+            hideCloseButton: true,
+            hideNextButton: true,
+            hideFooter: true,
+            data: {
+              autoNextOn: '#demographics',
+            },
+          },
+          {
+            target: '#demographics',
+            title: 'Demographics',
+            content:
+              'This is the Demographics section. Here you can find various fields and information related to the patient.',
+            disableBeacon: true,
+            hideBackButton: true,
+          },
+          {
+            target: '#contact',
+            title: 'Contact Details',
+            content: "Here you can update the patient's contact information.",
             disableBeacon: true,
           },
           {
-            target: '[data-extension-id="clinical-appointments-dashboard-link"]',
-            title: 'Click on this link. This step is configured to be automatic and will take you to the next step. Once the given query selector resolves an element on the page, it will proceed automatically.',
-            hideCloseButton: true,
-            hideNextButton: true,
-            disableOverlayClose: true,
-            spotlightClicks: true,
-            data: {
-              autoNextOn: '[data-extension-id="clinical-appointments-dashboard"]',
-            }
+            target: '#relationships',
+            title: 'Relationships',
+            content: "In this section, you can manage the patient's relationships.",
+            disableBeacon: true,
           },
           {
-            target: '[data-extension-id="clinical-appointments-dashboard"]',
-            title: 'Congrats! You have reached the clinical appointments dashboard.',
-            disableOverlayClose: true,
+            target: 'button[type="submit"]',
+            title: 'Register Patient',
+            content: "Click this button to register the patient's information into the system.",
+            disableBeacon: true,
           },
-          {
-            target: '[aria-label="OpenMRS"]',
-            title: 'Now, let’s see how this behaves when elements take a bit longer to load. Set your network throttling to "Slow 3G" and hit "Next".',
-            disableOverlayClose: true,
-            hideBackButton: true,
-          },
-          {
-            target: '[data-extension-id="laboratory-dashboard-link"]',
-            title: 'Let\'s navigate to the laboratory page. Our next target is the "Tests Ordered" table. I’ll disappear once you reach the laboratory page and reappear when the table is loaded. See you there!',
-            hideCloseButton: true,
-            hideNextButton: true,
-            disableOverlayClose: true,
-            spotlightClicks: true,
-            data: {
-              autoNextOn: '[data-extension-id="laboratory-dashboard"]',
-            }
-          },
-          {
-            target: '[data-extension-id="all-lab-requests-table"] table',
-            title: 'It\'s me again. By default, I\'ll wait for the element to appear, so you don\'t have to worry about slow components when writing a new tutorial.',
-            disableOverlayClose: true,
-            hideBackButton: true,
-          },
-          {
-            target: '[aria-label="OpenMRS"]',
-            title: 'Now let\'s do a fun exercise. Can you find out how to view a patient\'s allergies on your own? Feel free to turn off network throttling. ;) ',
-          },
-          {
-            target: '[data-extension-slot-name="patient-chart-allergies-dashboard-slot"]',
-            title: 'Great job! You found the allergies section! This is the end of the tutorial. Feel free to explore the system on your own or check out the other tutorials.',
-          }
-        ]
-      }
-    ]
-  }
+        ],
+      },
+    ],
+  },
 };
 
 export type Config = {
@@ -184,7 +174,7 @@ export type Config = {
       disableBeacon: boolean;
       data?: {
         autoNextOn?: boolean;
-      }
+      };
     }[];
   }[];
 };

c5beb706a243d24a7e7961534a003cab094a3cc5
Samuel Muli <samuelmuli95@gmail.com>
2024-10-17 13:52:35 +0300

    (fix) O3-3890 Help menu should use carbon design standards

--- src/tutorial/tutorial.tsx
+++ src/tutorial/tutorial.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { showModal } from '@openmrs/esm-framework';
-import { MenuItem } from '@carbon/react';
-import styles from './styles.scss';
+import styles from './styles.scss'
 
 const Tutorial = () => {
   const { t } = useTranslation();
@@ -15,11 +14,7 @@ const Tutorial = () => {
 
   return (
     <>
-      <MenuItem 
-        onClick={handleOpenModal}
-        label={t('tutorials', 'Tutorials')}
-        className={styles.tutorialMenuItem}
-      />
+      <div onClick={handleOpenModal} className={styles.tutorialsText}>{t('tutorials', 'Tutorials')}</div>
     </>
   );
 };

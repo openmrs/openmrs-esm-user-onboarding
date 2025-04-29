69683235774209ab65e10daf8813cb655a3783bc
Dennis Kigen <kigen.work@gmail.com>
2025-04-29 12:07:07 +0300

    (chore) Add placeholder type declarations for react-aria
    This is a temporary solution to avoid type errors in the react-aria library. See more info in this
    Slack thread: https://openmrs.slack.com/archives/C02UNMKFH8V/p1744895495794319.

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,16 @@ declare module '@carbon/react';
 declare module '*.css';
 declare module '*.scss';
 declare module '*.png';
+
+declare module 'react-aria' {
+  export const I18nProvider: (...args: any) => JSX.Element;
+  export type { DateValue } from '@react-types/datepicker';
+  export const mergeProps: any;
+  export const useLocale: any;
+  export const useDateField: any;
+  export const useDatePicker: any;
+  export const useDateSegment: any;
+  export const useFocusRing: any;
+  export const useHover: any;
+  export const useObjectRef: any;
+}
